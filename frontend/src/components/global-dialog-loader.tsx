f024fc96f3ca7277fa3e4ee019ad39d0da8d9586
e-labInnovations <mohammedashad23@gmail.com>
2025-08-01 23:06:34 +0530

    frontend: updated dialog component

--- frontend/src/components/global-dialog-loader.tsx
+++ frontend/src/components/global-dialog-loader.tsx
@@ -8,7 +8,7 @@ const GlobalDialogLoader = () => {
   return (
     <Dialog open={isLoading}>
       <DialogContent
-        hideCloseButton
+        showCloseButton={false}
         className="flex max-w-1/2 flex-col items-center justify-center gap-4 rounded-lg border-none py-8 text-center"
       >
         <Logo size={64} className="animate-pulse" />
